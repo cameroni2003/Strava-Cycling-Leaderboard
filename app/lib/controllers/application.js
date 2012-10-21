e40a008c0f4455a0adf89526ceda827ca3b028ac
Cameron I <cameron.isiminger@gmail.com>
2012-10-21 17:54:16 -0400

    implemented getting data from ember-data

--- app/lib/controllers/application.js
+++ app/lib/controllers/application.js
@@ -11,22 +11,5 @@ App.HomeBodyView = Em.View.extend({
 });
 
 App.ridesController = Ember.ArrayController.create({
-	content: [],
-	init: function () {
-		var self = this;
-		$.ajax({
-			url: 'http://query.yahooapis.com/v1/public/yql',
-			data: { format: 'json', q: 'select * from json where url="http://www.strava.com/api/v1/rides?clubId=3957"' },
-			dataType: 'jsonp',
-			success: function (data) {
-				var time = 2000;
-				var myAr = ['blah', 'meh', 'ok'];
-				self.set('content',data.query.results.json.rides);
-				
-			}
-		});
-	}
+	content: App.store.findQuery(App.Ride)
 });
-
-
-
--- app/lib/store.js
+++ app/lib/store.js
@@ -1,5 +1,37 @@
 require('ember-skeleton/core');
 
-App.store = DS.Store.create({
-  revision: 4
+App.adapter = DS.Adapter.create({
+	find: function(store, type, id)
+	{
+		var self = this;
+		$.ajax({
+			url: 'http://query.yahooapis.com/v1/public/yql',
+			data: { format: 'json', q: 'select * from json where url="http://www.strava.com/api/v1/rides?clubId=3957"' },
+			dataType: 'jsonp',
+			success: function (data) {
+				store.load(type, id, { id: '123', name: 'blah'});
+			}
+		});
+	},
+	findQuery: function(store, ids, query, modelArray) {
+		$.ajax({
+			url: 'http://query.yahooapis.com/v1/public/yql',
+			data: { format: 'json', q: 'select * from json where url="http://www.strava.com/api/v1/rides?clubId=3957"' },
+			dataType: 'jsonp',
+			success: function (data) {
+				modelArray.load(data.query.results.json.rides);
+			}
+		});
+	}
 });
+
+App.store = DS.Store.create({
+  revision: 6,
+  adapter: App.adapter
+});
+
+App.Ride = DS.Model.extend({
+	//id: DS.attr('number'),
+	name: DS.attr('string')
+});
+
